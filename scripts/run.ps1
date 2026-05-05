f98c53dcc55da9fb3d71cfa9afdbe495b7eeb1d8
Rastislav Pašek <rastinko50@gmail.com>
2026-05-05 19:04:47 +0200

    Add MongoDB compose setup

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,13 +10,29 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:MEDEDU_API_ENVIRONMENT = "Development"
 $env:MEDEDU_API_PORT = "8080"
+$env:MEDEDU_API_MONGODB_USERNAME = "root"
+$env:MEDEDU_API_MONGODB_PASSWORD = "neUhaDnes"
+$env:MEDEDU_API_MONGODB_PORT = "27018"
+$env:MEDEDU_API_MONGO_EXPRESS_PORT = "8082"
+
+function mongo {
+    docker compose --env-file ${ProjectRoot}/deployments/docker-compose/.env --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/mededu-api-service
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/mededu-api-service
+        } finally {
+            mongo down
+        }
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    "mongo" {
+        mongo up
     }
     default {
         throw "Unknown command: $command"
